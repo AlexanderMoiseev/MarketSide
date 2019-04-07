7129165aaa7d2a13f8539342efb00d2aa5de33cd
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2019-04-07 13:31:31 +0300

    initial commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+
+
+class App extends React.Component {
+    render() {
+        return ( 
+            <div className="App">
+                <h1>Hello232 !</h1>
+            </div>
+        )
+    }
+}
+export default App
