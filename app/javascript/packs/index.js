1c1e8330b6113ba5bf8993e8615710513c5d1f55
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2019-04-20 14:34:36 +0300

    r

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,39 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../components/App'
-import {fromJS} from 'immutable';
-import {applyMiddleware, createStore} from "redux"
-import {Provider} from 'react-redux'
+import { fromJS } from 'immutable';
+import { applyMiddleware, createStore } from "redux"
+import { Provider } from 'react-redux'
 import thunk from "redux-thunk"
 import reducers from '../reducers/index'
 
-  // Example Data
-  export var tableData = {
-    columns: ['Currency', 'Cost/Unit', 'Unit', 'Units Requested'],
-    rows: [{
-      'Currency': 'Monero',
-      'Cost/Unit': 50,
-      'Unit': '1 Hour',
-      'Units Requested': 12
-    }, {
-      'Currency': 'Bitcoin',
-      'Cost/Unit': 50,
-      'Unit': '1 Hour',
-      'Units Requested': 12
-    }, {
-      'Currency': 'USD',
-      'Cost/Unit': 50,
-      'Unit': '1 Hour',
-      'Units Requested': 12
-    }, {
-      'Currency': 'EURO',
-      'Unit': null,
-      'Cost/Unit': undefined,
-      'Units Requested': 42
-    }]
-  };
+// Example Data
+export var tableData = {
+  columns: ['Currency', 'Cost/Unit', 'Unit', 'Units Requested'],
+  rows: [{
+    'Currency': 'Monero',
+    'Cost/Unit': 50,
+    'Unit': '1 Hour',
+    'Units Requested': 12
+  }, {
+    'Currency': 'Bitcoin',
+    'Cost/Unit': 50,
+    'Unit': '1 Hour',
+    'Units Requested': 12
+  }, {
+    'Currency': 'USD',
+    'Cost/Unit': 50,
+    'Unit': '1 Hour',
+    'Units Requested': 12
+  }, {
+    'Currency': 'EURO',
+    'Unit': null,
+    'Cost/Unit': undefined,
+    'Units Requested': 42
+  }]
+};
 
-const initialState = { data:tableData, counter:2 }
+const initialState = { data: tableData, counter: 2 }
 
 const store = createStore(
   reducers, fromJS(initialState),
@@ -42,7 +42,7 @@ const store = createStore(
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Provider store={store}>
-      <App/>
+      <App />
     </Provider>,
     document.body.appendChild(document.createElement('div')),
   )
--- app/javascript/reducers/index.js
+++ app/javascript/reducers/index.js
@@ -1,13 +1,13 @@
-import {INC, ACTIVE_POLL} from '../actions/actionList';
-import {fromJS} from 'immutable';
+import { INC, ACTIVE_POLL } from '../actions/actionList';
+import { fromJS } from 'immutable';
 
 export default (state, action) => {
 
   if (action.type === INC) {
-  console.log(action.type)
-
+    console.log(action.type)
+    
     return state.mergeDeep({
-        counter: state.get("counter") + 1
+      counter: state.get("counter") + 1
     });
   }
 
